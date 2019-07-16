52520f1c8307fd03d42223445bd523ad7e1b9ca7
Aditya Agarwal <adityag2511@gmail.com>
2019-07-17 05:13:04 +0530

    DB docker setup

--- start.sh
+++ start.sh
@@ -8,4 +8,15 @@ docker run -dt --name rss-python \
 
 docker exec rss-python conda update -c base -c defaults conda
 docker exec rss-python conda install beautifulsoup4
+
+docker run -d --name rss-postgres \
+ -e POSTGRES_DB=audioboom \
+ -e POSTGRES_PASSWORD=parserssfeed \
+ -p 5432:5432 \
+ -v $(PWD):/home \
+ -w /home  \
+ postgres
+
+docker exec -it rss-postgres psql -U postgres -f create_db.sq
+
 docker exec rss-python python main.py
